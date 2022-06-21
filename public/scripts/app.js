96e2b030610d432e4154740a5a15d30ae952fd7d
Cxlopez <cristian4402@hotmail.com>
2022-06-21 12:58:07 -0400

    functinon comment

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-undef */
 // Client facing scripts here
+
+//Error messages for missing user info: email, title, option, etc..
 $(document).ready(function() {
 
   $('form').on('click', 'button', function(event) {
