9de3d5775ab5807d0374e037f0aec40b21398b4e
donnahuang <donnaphzj@gmail.com>
2022-06-20 22:24:32 -0400

    create poll endpoint

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -13,7 +13,7 @@ CREATE TABLE options (
   id SERIAL PRIMARY KEY NOT NULL,
   poll_id  INTEGER REFERENCES polls(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NOT NULL
+  description VARCHAR(255)
 );
 
 
