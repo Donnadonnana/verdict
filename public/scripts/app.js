c0f2906ac897aebd3428e69ecd7bfc36e8c51213
Cxlopez <cristian4402@hotmail.com>
2022-06-21 01:51:34 -0400

    JQuery for alert user of missing info

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,31 @@
+/* eslint-disable no-undef */
 // Client facing scripts here
+$(document).ready(function() {
+
+  $('form').on('click', 'button', function(event) {
+    event.preventDefault();
+
+    $('.alert-user').slideUp();
+    if ($('#email-text').val().length <= 1) {
+      $('.alert-user').slideDown(750).text('You need an email 🙄');
+      return;
+    }
+
+    if ($('#title-text').val().length <= 1) {
+      $('.alert-user').slideDown(750).text('You have not created a title, don\'t be shy!   😎');
+      return;
+    }
+
+    if ($('#poll-question-1').val().length <= 1) {
+      $('.alert-user').slideDown(750).text('Can\'t vote without options give atleast 2!');
+      return;
+    }
+
+    if ($('#poll-question-2').val().length <= 1) {
+      $('.alert-user').slideDown(750).text('Can\'t vote without options give atleast 2!');
+      return;
+    }
+
+  });
+
+});
